5e84f514d1ed5bea7457209e914dd5cd6561b26b
FredJenningsAstro <s2220800@ed.ac.uk>
2023-05-18 19:30:25 +0100

    Added instr_files

--- moxha/__init__.py
+++ moxha/__init__.py
@@ -1,1 +1,16 @@
-from . import utils
+from . import postprocess
+from . import tools
+from . import observations
+
+try:
+    from . import spectralfits
+except:
+    pass
+try:
+    from . import profilefits 
+except:
+    pass
+try:
+    from . import tessmaps       
+except:
+    pass
