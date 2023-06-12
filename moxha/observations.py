fd099103243ccc6dd4cd821adba4c1c8ee2058db
FredJenningsAstro <s2220800@ed.ac.uk>
2023-06-12 19:46:33 +0100

    will now record particle positions

--- moxha/observations.py
+++ moxha/observations.py
@@ -989,7 +989,7 @@ class Observation:
         lumin_field = str(f"xray_luminosity_{self.emin_for_EW_values}_{self.emax_for_EW_values}_keV")
         emis_field = str(f"xray_emissivity_{self.emin_for_EW_values}_{self.emax_for_EW_values}_keV")
         radius = (ptype,"radius")
-        
+        np.save(f"{yt_data_path}/{self._idx_tag}_filtered_gas_coords.npy",self.sp[ptype, "particle_position"], allow_pickle=True,)
         # for field in self.ds.derived_field_list:
         #     # if field[0] == ptype:
         #         print(field)
